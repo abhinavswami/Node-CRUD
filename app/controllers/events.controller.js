6c8a537ae2d650a004850abb81a0f1c4a08cc909
Abhinav Swami <abhinav.swami21@gmail.com>
2017-08-05 13:28:36 -0700

    Add form validation

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -67,13 +67,25 @@ function showSingle(req,res){
  * Show the create form
  */
  function showCreate(req,res){
-   res.render('pages/create');
+   res.render('pages/create', {
+     errors: req.flash('errors')
+   });
  }
 
  /**
   * Process the create form
   */
   function processCreate(req,res){
+    // validate informtion
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('description', 'Description is required').notEmpty();
+
+    // if there are errors, redirect and save errors to flash
+    const errors = req.validationErrors();
+    if(errors){
+      req.flash('errors', errors.map(err => err.msg));
+      return res.redirect('/events/create');
+    }
     // create a new event
     const event = new Event({
       name: req.body.name,
